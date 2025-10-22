9d764ea8f635b8f3cb579adea11145c92c5a4b12
Davon Daniels <38470284+felician23@users.noreply.github.com>
2025-10-21 22:30:17 -0500

    Add files via upload

---
+++ budgetapp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cout << "Welcome to my first budget app" << endl;
+	cout << "lets start with your Monthly Gross income: ";
+	
+	double gross_pay;
+	double utility_electric;
+	double utility_water;
+	double utility_gas;
+	
+	cin >> gross_pay;
+
+}
